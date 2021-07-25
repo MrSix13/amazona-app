4a0c7e0c1b89e3de83995319c524b7074a0a1bd5
MrSix13 <bastiangarcesga@gmail.com>
2021-07-25 15:19:25 -0400

    tercero

--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -1,10 +1,20 @@
 import Product from "../components/Product";
-import data  from "../data";
+import { useState,useEffect } from "react";
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products,setProducts]=useState([]);
+
+  useEffect(()=>{
+      const fetchData = async ()=>{
+         const {data} = await axios.get('/api/products');
+         setProducts(data)
+      }
+      fetchData();
+  },[])
     return (
         <div className="row center">
-                {data.products.map((product)=>(
+                {products.map((product)=>(
                   <Product key={product._id} product = {product}/>
                 ))}
         </div>
