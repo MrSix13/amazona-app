d47862a66f36932f14be6a5ca41c5f3ed7494396
MrSix13 <bastiangarcesga@gmail.com>
2021-07-24 21:10:26 -0400

    main

---
+++ Frontend/src/data.js
@@ -0,0 +1,39 @@
+const data ={
+    products:[
+        {
+            _id:'1',
+            name:'Battlefield 2042',
+            category: 'Shooter',
+            image:'images/battlefield.jpg',
+            price: 190,
+            brand:'EA',
+            numReviews:10,
+            rating: 4.5,
+            description: 'New Shooter game by EA'
+        },
+        {
+            _id:'2',
+            name:'Call Of Duyt: Cold War',
+            category: 'Shooter',
+            image:'images/coldwar.jpg',
+            price: 90,
+            brand:'Activision',
+            numReviews:10,
+            rating: 5,
+            description: 'New Shooter game by Activision'
+        },
+        {
+            _id:'3',
+            name:'Fifa 22',
+            category: 'Arcade',
+            image:'images/fifa22.jpg',
+            price: 60,
+            brand:'EA',
+            numReviews:10,
+            rating: 4.5,
+            description: 'New Shooter game by EA'
+        }
+    ]
+}
+
+export default data;
