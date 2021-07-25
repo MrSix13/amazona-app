ff3abf90564247225c7e982a98144a42a35bac28
MrSix13 <bastiangarcesga@gmail.com>
2021-07-25 00:38:36 -0400

    tercero

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,28 @@
-import Product from "./components/Product";
-import data  from "./data";
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
-    <div className="grid-container">
-        <header className="row">
-            <div>
-                <a className="brand" href="/">Amazona</a>
-            </div>
-            <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign In</a>
-            </div>
-        </header>
-        <main>
-          <div className="row center">
-              {data.products.map((product)=>(
-                <Product key={product._id} product = {product}/>
-              ))}
-          </div>
-        </main>
-        <footer className="row center">All Right Reserved</footer>
-    </div>
+    <Router>
+      <div className="grid-container">
+          <header className="row">
+              <div>
+                  <a className="brand" href="/">Amazona</a>
+              </div>
+              <div>
+                  <a href="/cart">Cart</a>
+                  <a href="/signin">Sign In</a>
+              </div>
+          </header>
+          <main>
+            <Route path="/product/:id"component={ProductScreen}/>
+            <Route path="/" exact component={HomeScreen}/>
+
+          </main>
+          <footer className="row center">All Right Reserved</footer>
+      </div>
+    </Router>
   );
 }
 
