8daf831b330676dc5bb6bcf9aac5a87bba937c3b
MrSix13 <bastiangarcesga@gmail.com>
2021-07-25 15:43:55 -0400

    quinto

--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -1,23 +1,42 @@
 import Product from "../components/Product";
 import { useState,useEffect } from "react";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 import axios from 'axios';
 
 const HomeScreen = () => {
   const [products,setProducts]=useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(()=>{
+    
       const fetchData = async ()=>{
-         const {data} = await axios.get('/api/products');
-         setProducts(data)
+        try {
+            setLoading(true);
+            const {data} = await axios.get('/api/products');
+            setLoading(false);
+            setProducts(data);
+        } catch (err) {
+            setError(err.message);
+            setLoading(false);
+        }
       }
       fetchData();
   },[])
     return (
-        <div className="row center">
-                {products.map((product)=>(
-                  <Product key={product._id} product = {product}/>
-                ))}
-        </div>
+      <div>
+          { loading? <LoadingBox></LoadingBox>
+              :
+              error?<MessageBox variant="danger">{error}</MessageBox>
+              :
+              <div className="row center">
+                      {products.map((product)=>(
+                        <Product key={product._id} product = {product}/>
+                      ))}
+              </div>
+          }
+      </div>
     )
 }
 
